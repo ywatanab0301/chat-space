5598c3c5636a3889f298e357b28cb6b4d6837678
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-24 16:27:41 +0900

    Display sidebar group info and chat page's group info
    1. Define show_last_message in Group model.
    2. get info of group and group members and display them in the view.

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,13 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :members
   has_many :users, through: :members
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
