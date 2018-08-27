82a1f0d1c7012d3f201d91796dba2a8af5227536
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-27 15:30:44 +0900

    Asynchronous communication
    Get the url which the data is sent when the message is submitted, and
    with the asynchronous communication by ajax make the create action work

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,6 +2,14 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'jason',
+      processData: false,
+      contentType: false,
+    })
   })
 })
