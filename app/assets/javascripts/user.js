2ee8ed997a5cd3200dd701363bd8340cc5e096d9
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-28 19:20:06 +0900

    Implement incremental search function
    - edit text field
    - create user.js file
    - edit user.js file

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,50 @@
+$(function(){
+
+var search_list = $("#user-search-result");
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.name}">追加</a>
+              </div>`
+  search_list.append(html);
+}
+
+function appendNoUser(user) {
+  var html = `<div class='chat-group-user clearfix'>${ user }</div>`
+  search_list.append(html);
+}
+
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert("ユーザー検索に失敗しました");
+    })
+  });
+});
+
+$(function(){
+  $().on("click", function() {
+
+  })
+})
