794feb2e096886359ac6e882c6a485239625ea15
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-29 13:01:01 +0900

    edit user.js and view file
    Clicking add button ignite event that delete the user from incremental
    search result and append the selected user in the members' list.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -44,7 +44,24 @@ function appendNoUser(user) {
 });
 
 $(function(){
-  $().on("click", function() {
 
+var member_list = $(".chat-group-users")
+
+function appendMember(member_name, member_user_id) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+              <input name='group[user_ids][]' type='hidden' value='${member_user_id}'>
+              <p class='chat-group-user__name'>${member_name}</p>
+              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+            </div>`
+member_list.append(html)
+}
+
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log(this);
+    $(this).parent().remove();
+    var member_name = $(this).attr('data-user-name')
+    var member_user_id = $(this).attr('data-user-id')
+    console.log(member_name, member_user_id)
+    appendMember(member_name, member_user_id)
   })
 })
