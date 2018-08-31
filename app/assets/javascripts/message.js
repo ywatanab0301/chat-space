740b5828f78843826c6714bcc7292255f13a5f1b
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-31 16:38:02 +0900

    Automatic update
    1. message.js
    Set the variance of “interval” by using  setInterval method, and read
    the autoUpdate function every 5 seconds unless the user stays at the
    same chat page.
    
    2. message.js / _message.html.haml
    make function autoUpdate
    - if class “messages” has messages, send the very last message’s id
    to the controller as json datatype, and if there is no messages, the id
    “0” is sent.
    
    3. messages_controller.rb
    - with respond_to divide the process by the data type html or json.
    - if the json format, when the newer id than the id of current
    viewing chat page’s very last message does not exist on the database,
    controller deliver the data of the @new_messages.
    
    4. index.json.jbuilder
    - json data received in the controller is sent throught jbuilder, and
    define those data in the jbuilder for use in message.js file.
    
    5. message.js
    - receive the data from jbuilder
    - if success, the new messages are inserted to the very last place of
    “.messages”.
    - if fail, show alert

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
+
 $(function(){
   function buildHTML(message){
     var image = message.image !== null ? `<img class="lower-message__image" src=${message.image}></img>`:"";
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__username">${message.name}</div>
                   <div class="message__time">${message.time}</div>
                   <div class="message__text">${message.body}</div>
@@ -9,6 +10,11 @@ $(function(){
                 </div>`
     return html;
   }
+
+  function scrollDown(){
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -27,10 +33,49 @@ $(function(){
       $('.footer__input--send-button').prop('disabled', false)
       $('.footer__input--box').val('');
       $('.hidden').val('');
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      scrollDown();
     })
     .fail(function(){
-      alert('error');
+      alert('エラー');
     })
   })
+
+    function autoUpdate(){
+      if($('.messages')[0]){
+        // 現在表示されているページ中、最後のクラス名messageのmessage−idを取得しidに代入
+        var message_id = $('.message:last').data('message-id');
+      } else {
+        var message_id = 0;
+      }
+      // ajaxで現在のページの情報をjson形式で送信
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: { message: { id: message_id } },
+        dataType: 'json'
+      })
+      // jbuilderで送られてきたデータを受ける
+      .done(function(jason){
+        var insertHTML = '';
+        jason.forEach(function(message) {
+          if (message.id > message_id) {
+          insertHTML += buildHTML(message);
+         }
+        })
+        $('.messages').append(insertHTML);
+        scrollDown();
+      })
+      .fail(function(jason){
+        alert('自動更新に失敗しました。')
+      })
+    }
+
+  var interval = setInterval(function(){
+    // urlが違っていたら自動更新は中止
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      autoUpdate();
+    } else{
+      clearInterval(interval)
+    }
+  } , 5000 )
 })
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json { @new_messages = @messages.where('id > ?', params[:message][:id]) }
+    end
   end
 
   def create
